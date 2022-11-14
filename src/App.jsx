86391a1945918ec4fb4a71647ede45448b87cd20
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-14 15:09:06 -0500

    axios request written

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import {useState} from 'react';
+import axios from 'axios';
 
 const App = () => {
   const [showRunners, setShowRunners] = useState(false);
 
   const displayRunnerInfo = () => {
     setShowRunners(!showRunners);
-  }
+  };
+
+  const getRunnerInfo = () => {
+    axios.get('/runners')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log('error getting runner info: ', err);
+      })
+  };
 
 
   return (
@@ -16,6 +27,6 @@ const App = () => {
       {showRunners && <div>modal here</div>}
     </div>
     );
-  }
+  };
 
   export default App;
