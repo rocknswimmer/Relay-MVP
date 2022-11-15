453603624b53b942f57eda13e1b22d103e68d988
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-14 19:21:28 -0500

    added keys

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ const App = () => {
         <div>
           {runners.map((runner, i) => {
             return (<Accordion
+              key={i}
               title={'Runner ' + runner.id}
               content={
                 <div>
