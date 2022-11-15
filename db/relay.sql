3d94f9521241c08e547616df28b9a814f8186954
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-15 10:31:46 -0500

    redisigned runner table and added legs table

--- db/relay.sql
+++ db/relay.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS runners;
 
 CREATE TABLE runners (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   runner VARCHAR(100) NOT NULL DEFAULT NULL,
-  phone VARCHAR(10)
+  phone VARCHAR(10),
+  pace REAL
+);
+
+CREATE TABLE legs (
+  id SERIAL,
+  runner REFERENCES runners (id),
+  distance REAL
 );
 
