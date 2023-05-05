46c4f826e6e3d4b86a396e8824ad5e80c326b159
Andy Simmons <rocknswimmer116@gmail.com>
2023-05-05 14:01:09 -0400

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -111,3 +111,10 @@ const App = () => {
   };
 
   export default App;
+
+  /*
+  minimal wrap to dos
+  individual runner log in and my log in (maybe login button instead of whose viewing section)
+  time zones handled easier in db on client/server side
+  store variables in browser for one time login?
+  */
