13a9827a91df915efa90bf2e21c50c4bad06a6e1
Andy Simmons <rocknswimmer116@gmail.com>
2024-10-15 13:25:31 -0400

    cleaned up through public

--- db/relay.sql
+++ db/relay.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS legs;
-DROP TABLE IF EXISTS runners;
+DROP TABLE IF EXISTS legs1;
+DROP TABLE IF EXISTS runners1;
 
-CREATE TABLE runners (
+CREATE TABLE runners1 (
   id SERIAL PRIMARY KEY,
   runner VARCHAR(100) NOT NULL DEFAULT NULL,
   phone VARCHAR(10) NOT NULL DEFAULT NULL,
   pace REAL NOT NULL DEFAULT NULL
 );
 
-CREATE TABLE legs (
+CREATE TABLE legs1 (
   id SERIAL PRIMARY KEY,
   runner INTEGER NOT NULL DEFAULT NULL REFERENCES runners (id),
   distance REAL NOT NULL DEFAULT NULL,
@@ -20,13 +20,13 @@ CREATE TABLE legs (
   dif VARCHAR(50) NOT NULL DEFAULT 'TBD'
 );
 
-insert into runners (runner, phone, pace) values ('And e', '5554567890', 10) returning *;
-insert into runners (runner, phone, pace) values ('And 2', '5554567891', 10.5) returning *;
-insert into runners (runner, phone, pace) values ('And 3', '5554567892', 10.25) returning *;
+insert into runners1 (runner, phone, pace) values ('And e', '5554567890', 10) returning *;
+insert into runners1 (runner, phone, pace) values ('And 2', '5554567891', 10.5) returning *;
+insert into runners1 (runner, phone, pace) values ('And 3', '5554567892', 10.25) returning *;
 
-insert into legs (runner, distance) values (1, 3.1) returning *;
-insert into legs (runner, distance) values (2, 6.2) returning *;
-insert into legs (runner, distance) values (3, 5) returning *;
-insert into legs (runner, distance) values (1, 3.1) returning *;
-insert into legs (runner, distance) values (2, 6.2) returning *;
-insert into legs (runner, distance) values (3, 5) returning *;
+insert into legs1 (runner, distance) values (1, 3.1) returning *;
+insert into legs1 (runner, distance) values (2, 6.2) returning *;
+insert into legs1 (runner, distance) values (3, 5) returning *;
+insert into legs1 (runner, distance) values (1, 3.1) returning *;
+insert into legs1 (runner, distance) values (2, 6.2) returning *;
+insert into legs1 (runner, distance) values (3, 5) returning *;
